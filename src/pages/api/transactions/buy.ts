9e1fc16bf92df257a1f7ede67055490465c15d0b
Jakub Chelchowski <jakubchelchowski@gmail.com>
2022-11-24 23:35:46 +0100

    Error check

--- src/pages/api/transactions/buy.ts
+++ src/pages/api/transactions/buy.ts
@@ -54,6 +54,6 @@ async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
     });
     return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "Internal Server Error", extra: error });
   }
 }
